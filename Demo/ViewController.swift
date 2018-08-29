d1e825c6eab12b511d160d35c17e2dcc81069efa
CHETAN <chaudhari.c03@gmail.com>
2018-08-29 11:46:15 +0530

    Chechout branch checking

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -30,7 +30,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        
+        print("Check out Git")
         print("ABCD")
 
         print(name.name)
